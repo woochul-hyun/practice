fc91de4e845f021ff444a2e67fb68538e86f4508
Woochul Hyun <woochul.hyun91@gmail.com>
2019-03-14 21:44:27 +0900

    Todo list version2

--- 190314/190314TodoList.js
+++ 190314/190314TodoList.js
@@ -1,50 +1,125 @@
-function addTodoItem() {
-    var todoItem = $("#new-todo-item").val();
-    $("#todo-list").append(
-        "<li><input type='checkbox'" +
-            " name='todo-item-done'" +
-            " class='todo-item-done'" +
-            " value='" +
-            todoItem +
-            "' /> " +
-            todoItem +
-            " <button class='todo-item-delete'>" +
-            "Delete</button></li>"
-    );
+var totalItems = 0;
 
-    $("#new-todo-item").val("");
+function renameItem() {
+    var newText = prompt("Todo list 수정");
+    if (!newText || newText === "" || newText === " ") {
+        return false;
+    }
+    document.getElementById("item_" + totalItems).innerText = newText;
 }
 
-function deleteTodoItem(e, item) {
-    e.preventDefault();
-    $(item)
-        .parent()
-        .fadeOut("slow", function() {
-            $(item)
-                .parent()
-                .remove();
-        });
+function removeItem() {
+    var listItemId = this.id.replace("li_", "");
+    var removeEle = document.getElementById("li_" + listItemId);
+    removeEle.parentNode.removeChild(removeEle);
 }
 
-function completeTodoItem() {
-    $(this)
-        .parent()
-        .toggleClass("strike");
+function deleteItem() {
+    var listItemId = this.id.replace("removeBtn_" + "");
+    var removeEle = document.getElementById("li_" + listItemId);
+    removeEle.parentNode.removeChild(removeEle);
 }
 
-$(function() {
-    $("#add-todo-item").on("click", function(e) {
-        e.preventDefault();
-        addTodoItem();
-    });
+function updateItemStatus() {
+    var chId = this.id.replace("cb_", "");
+    var itemText = document.getElementById("item_" + chId);
 
-    //EVENT DELEGATION
-    //#todo-list is the event handler because .todo-item-delete doesn't exist when the document loads, it is generated later by a todo entry
-    //https://learn.jquery.com/events/event-delegation/
-    $("#todo-list").on("click", ".todo-item-delete", function(e) {
-        var item = this;
-        deleteTodoItem(e, item);
-    });
+    if (this.checked) {
+        itemText.className = "checked";
+    } else {
+        itemText.className = "";
+    }
+}
 
-    $(document).on("click", ".todo-item-done", completeTodoItem);
-});
+function addNewItem(list, itemText) {
+    totalItems++;
+
+    var date = new Date();
+    var id =
+        "" +
+        date.getHours() +
+        date.getMinutes() +
+        date.getSeconds() +
+        date.getMilliseconds();
+
+    var listItem = document.createElement("li");
+    listItem.id = "li_" + id;
+    listItem.className = "listItemClass";
+    listItem.ondblclick = removeItem;
+
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkBox";
+    checkBox.id = "cb_" + totalItems;
+    checkBox.onclick = moveItem;
+
+    var span = document.createElement("span");
+    span.id = "item_" + totalItems;
+    span.innerText = itemText;
+
+    var button = document.createElement("button");
+    button.id = "editBtn";
+    button.value = "edit";
+    button.innerText = "Edit";
+    button.onclick = renameItem;
+
+    var removeBtn = document.createElement("button");
+    removeBtn.id = "removeBtn_" + id;
+    removeBtn.className = "removeBtnClass";
+    removeBtn.value = "remove";
+    removeBtn.innerText = "Remove";
+    removeBtn.onclick = deleteItem;
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(span);
+    list.appendChild(listItem);
+    listItem.appendChild(removeBtn);
+    listItem.appendChild(button);
+}
+
+var inputText = document.getElementById("inputText");
+inputText.focus();
+
+//var btnNew = document.getElementById("btnAdd");
+//btnNew.onclick = function() {
+//    var itemText = inputText.value;
+//    if (!itemText || itemText === "" || itemText === " ") {
+//        return false;
+//    }
+//    addNewItem(document.getElementById("todolist"), itemText);
+//    inputText.focus();
+//    inputText.value = null;
+//};
+
+inputText.onkeyup = function(event) {
+    if (event.which === 13) {
+        var itemText = inputText.value;
+        if (!itemText || itemText === "" || itemText === " ") {
+            return false;
+        }
+        addNewItem(document.getElementById("todolist"), itemText);
+        inputText.focus();
+        inputText.value = null;
+    }
+};
+
+var donelist = document.getElementById("donelist");
+function moveItem() {
+    var listItemId = this.id.replace("cb_", "");
+    var listItem = document.getElementById("li_" + listItemId);
+
+    var chId = this.id.replace("cb_", "");
+    var itemText = document.getElementById("item_" + chId);
+
+    if (this.checked) {
+        itemText.className = "checked";
+    } else {
+        itemText.className = "";
+    }
+
+    var listItemParentId = listItem.parentElement;
+    if (listItemParentId == donelist) {
+        todolist.appendChild(listItem);
+    } else {
+        donelist.appendChild(listItem);
+    }
+}
