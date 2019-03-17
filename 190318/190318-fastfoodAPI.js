644eea9d765a06bb1a2383e7dd629a3507f69527
Woochul Hyun <woochul.hyun91@gmail.com>
2019-03-18 00:55:00 +0900

    add feat search
    search with click and enter

--- 190318/190318-fastfoodAPI.js
+++ 190318/190318-fastfoodAPI.js
@@ -2,27 +2,41 @@ var API_URL = "https://floating-harbor-78336.herokuapp.com/fastfood";
 
 $(function() {
     $(".btn-search").click(function() {
-        $.get(API_URL, {}, function(data) {
-            var list = data.list;
-            var total = data.total;
+        var searchKeyword1 = $("#text-search").val();
 
-            $(".total ").html("총" + total + "개의 패스트푸드점을 찾았습니다.");
+        search(searchKeyword1);
+    });
 
-            var $list = $(".list");
+    $("#text-search").on("keypress", function(e) {
+        if (e.keyCode === 13) {
+            var searchKeyword1 = $("#text-search").val();
 
-            for (var i = 0; i < list.length; i++) {
-                var item = list[i];
-
-                var $elem = $("#item-template")
-                    .clone()
-                    .removeAttr("id");
-
-                $elem.find(".item-no").html(i + 1);
-                $elem.find(".item-name").html(item.name);
-                $elem.find(".item-addr").html(item.addr);
-
-                $list.append($elem);
-            }
-        });
+            search(searchKeyword1);
+        }
     });
 });
+
+function search(searchKeyword1) {
+    $.get(API_URL, { searchKeyword: searchKeyword1 }, function(data) {
+        var list = data.list;
+        var total = data.total;
+
+        $(".total ").html("총" + total + "개의 패스트푸드점을 찾았습니다.");
+
+        var $list = $(".list");
+
+        for (var i = 0; i < list.length; i++) {
+            var item = list[i];
+
+            var $elem = $("#item-template")
+                .clone()
+                .removeAttr("id");
+
+            $elem.find(".item-no").html(i + 1);
+            $elem.find(".item-name").html(item.name);
+            $elem.find(".item-addr").html(item.addr);
+
+            $list.append($elem);
+        }
+    });
+}
