e2c1d85bd520c2897500c1ba8e1253331ee88cee
Woochul Hyun <woochul.hyun91@gmail.com>
2019-03-19 00:48:20 +0900

    finish fastfood

--- 190318/190318-fastfoodAPI.js
+++ 190318/190318-fastfoodAPI.js
@@ -2,41 +2,117 @@ var API_URL = "https://floating-harbor-78336.herokuapp.com/fastfood";
 
 $(function() {
     $(".btn-search").click(function() {
-        var searchKeyword1 = $("#text-search").val();
-
-        search(searchKeyword1);
+        var searchKeyword = $("#txt-search").val();
+        search(1, null, searchKeyword);
     });
 
-    $("#text-search").on("keypress", function(e) {
+    $("#txt-search").on("keypress", function(e) {
         if (e.keyCode === 13) {
-            var searchKeyword1 = $("#text-search").val();
-
-            search(searchKeyword1);
+            $(".btn-search").trigger("click");
         }
     });
+    $("#txt-search").focus();
 });
 
-function search(searchKeyword1) {
-    $.get(API_URL, { searchKeyword: searchKeyword1 }, function(data) {
-        var list = data.list;
-        var total = data.total;
+function search(page, perPage, searchKeyword) {
+    if (typeof page !== "number" || page < 1) page = 1;
 
-        $(".total ").html("총" + total + "개의 패스트푸드점을 찾았습니다.");
+    if (typeof perPage !== "number" || perPage <= 0) perPage = 10;
 
-        var $list = $(".list");
+    $.get(
+        API_URL,
+        {
+            page: page,
+            perPage: perPage,
+            searchKeyword: searchKeyword
+        },
+        function(data) {
+            var list = data.list;
+            var total = data.total;
 
-        for (var i = 0; i < list.length; i++) {
-            var item = list[i];
+            $(".total").html("총 " + total + "개의 패스트푸드점을 찾았습니다.");
 
-            var $elem = $("#item-template")
-                .clone()
-                .removeAttr("id");
+            var $list = $(".list").empty();
 
-            $elem.find(".item-no").html(i + 1);
-            $elem.find(".item-name").html(item.name);
-            $elem.find(".item-addr").html(item.addr);
+            for (var i = 0; i < list.length; i++) {
+                var item = list[i];
 
-            $list.append($elem);
+                var $elem = $("#item-template")
+                    .clone()
+                    .removeAttr("id");
+
+                var no = (page - 1) * perPage + i + 1;
+
+                $elem.find(".item-no").html(no);
+                $elem.find(".item-name").html(item.name);
+                $elem.find(".item-addr").html(item.addr);
+
+                $list.append($elem);
+            }
+
+            showPaging(page, perPage, total, searchKeyword);
         }
-    });
+    );
+}
+
+function showPaging(page, perPage, total, searchKeyword) {
+    var $paging = $(".paging").empty();
+    var numPages = 5;
+    var pageStart = Math.floor((page - 1) / numPages) * numPages + 1;
+    var pageEnd = pageStart + numPages - 1;
+    var totalPages = Math.floor(total / perPage) + 1;
+
+    if (pageEnd > totalPages) pageEnd = totalPages;
+
+    var prevPage = pageStart - 1;
+
+    if (prevPage < 1) prevPage = 1;
+
+    var nextPage = pageEnd + 1;
+
+    if (nextPage > totalPages) nextPage = totalPages;
+
+    var $prevElem = $(
+        '<a href="javascript:search(' +
+            prevPage +
+            "," +
+            perPage +
+            ",'" +
+            searchKeyword +
+            "')\">이전</a>"
+    );
+    $prevElem.addClass("prev");
+    $paging.append($prevElem);
+
+    for (var i = pageStart; i <= pageEnd; i++) {
+        var $elem = $(
+            '<a href="javascript:search(' +
+                i +
+                "," +
+                perPage +
+                ",'" +
+                searchKeyword +
+                "')\">" +
+                i +
+                "</a>"
+        );
+
+        if (i === page) {
+            $elem.addClass("current");
+        }
+
+        $paging.append($elem);
+    }
+
+    var $nextElem = $(
+        '<a href="javascript:search(' +
+            nextPage +
+            "," +
+            perPage +
+            ",'" +
+            searchKeyword +
+            "')\">다음</a>"
+    );
+    $nextElem.addClass("next");
+    $paging.append($nextElem);
 }
