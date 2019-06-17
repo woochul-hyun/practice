2a8bee993274d8cc7c317936fe635e9e841bbe4c
Woochul Hyun <woochul.hyun91@gmail.com>
2019-06-17 20:29:22 +0900

    exercise

---
+++ assignment/연습2/tabsUIAngular/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tabs = [
+    {
+      title: 'HTML',
+      active: true,
+      iconClass: 'fab fa-html5',
+      content: `<strong>HTML(HyperText Markup Language)</strong> is the most basic building block of the Web.
+        It describes and defines the content of a webpage along with the basic layout of the webpage.
+        Other technologies besides HTML are generally used to describe a web page's
+        appearance/presentation(CSS) or functionality/ behavior(JavaScript).`
+    },
+    {
+      title: 'CSS',
+      active: false,
+      iconClass: 'fab fa-css3',
+      content: `<strong>Cascading Style Sheets(CSS)</strong> is a stylesheet language used to describe
+        the presentation of a document written in HTML or XML (including XML dialects
+        such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen,
+        on paper, in speech, or on other media.`
+    },
+    {
+      title: 'JavaScript',
+      active: false,
+      iconClass: 'fab fa-js-square',
+      content: `<strong>JavaScript(JS)</strong> is a lightweight interpreted or JIT-compiled programming
+        language with first-class functions. While it is most well-known as the scripting
+        language for Web pages, many non-browser environments also use it, such as Node.js,
+        Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm,
+        dynamic language, supporting object-oriented, imperative, and declarative
+        (e.g. functional programming) styles.`
+    }
+  ];
+
+  changeNav(i: number) {
+    this.tabs = this.tabs.map((item, index) => {
+      return i === index
+        ? { ...item, active: true }
+        : { ...item, active: false };
+    });
+  }
+
+  title = 'tabsUI';
+}
